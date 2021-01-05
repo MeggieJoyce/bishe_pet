8e9c59eb59add18df9ebfb0d52d451ab66c303f6
Joycelyj <liaoyuanjing@163.com>
2021-01-05 20:04:48 +0800

    第一次提交

---
+++ pages/demo/demo.vue
@@ -0,0 +1,48 @@
+<template>
+	<view >
+		<view class="box">
+			box
+		</view>
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isshow:true
+				}
+			}
+		},
+		methods: {
+		changeShow:function(){
+			
+		}
+			// changeUsername:function(name){
+			// 	this.userinfo.username="小红"+name;
+			// }
+		}
+	}
+</script>
+
+<style>
+	.box{
+		height: 300px;
+		width: 350px;
+		border: 1upx soild #333333;
+		background: #F0AD4E;
+		display: flex;
+	
+	}
+/* .box>view{
+	border: 1upx solid #F2F2F2;
+	background: #F0AD4E;
+	color: #FFFFFF;
+	height: 250upx;
+	width: 350upx;
+	line-height: 200upx;
+	font-size: 30px;
+	font-weight: bold;
+	text-align: center;
+} */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+
+<text class="iconfont icon-shouye" style="font-size: 100rpx;"></text>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
